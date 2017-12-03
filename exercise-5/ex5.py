38172590a84a49dfc8e50b95530111e0a730699a
matthiasxc <matthias.shapiro@gmail.com>
2017-12-02 21:25:44 -0800

    adding exercise 5

---
+++ exercise-5/ex5.py
@@ -0,0 +1,21 @@
+name = 'Matthias Shapiro'
+age = 32
+height = 64
+weight = 195
+eyes = 'Green'
+teeth = 'Off-White'
+hair = 'Brown'
+heightInCm = height * 2.54
+weightinKg = weight * 0.4536
+
+print(f"Let's talk about {name}.")
+print(f"He's {height} inches tall.")
+print(f"That is {heightInCm} centimeters tall")
+print(f"He's {weight} pounds heavy.")
+print(f"That wouljd be {weightinKg} kilograms")
+print("Actually that's not too heavy.")
+print(f"He's got {eyes} eyes and {hair} hair.")
+print(f"His teeth are usually {teeth} depending on the coffee.")
+
+total = age + height + weight
+print(f"If I add {age}, {height}, and {weight} I get {total}.")
