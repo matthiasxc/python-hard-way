9c6dd7a32687239bc452801f0f0a59cf1a5a469f
matthiasxc <matthias.shapiro@gmail.com>
2017-12-02 19:47:12 -0800

    code through exercise 1

--- exercise-1/ex1.py
+++ exercise-1/ex1.py
@@ -1,2 +1,8 @@
-#testing some things
-#adding changes with sourcetree
+print("Hello World!")
+#print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print('Yay! Printing.')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
+print ("Making my script print another line with \"quotation\" marks")
